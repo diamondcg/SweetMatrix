4e32d26d9daab8af7e2c8275d358ccc44c742b2e
diamondcg <diamondcg@users.noreply.github.com>
2016-08-05 16:26:32 -0400

    Add files via upload

--- matrix.cpp
+++ matrix.cpp
@@ -1,7 +1,133 @@
 #include "matrix.h"
 #include <math.h>
+#include <stdio.h>
+#include <string>
+
+
+/******************************
+*			Vec2
+******************************/
+
+Vec2::Vec2(){
+	x = y = 0;
+}
+
+Vec2::Vec2(double _x, double _y){
+	x = _x;
+	y = _y;
+}
+Vec2::~Vec2(){
+}
+void Vec2::Print(){
+	printf("(%lf, %lf)\n", x, y);
+}
+Vec2 Vec2::Zero(){
+	return Vec2(0, 0);
+}
+Vec2 Vec2::One(){
+	return Vec2(1, 1);
+}
+Vec2 Vec2::XUnit(){
+	return Vec2(1, 0);
+}
+Vec2 Vec2::YUnit(){
+	return Vec2(0, 1);
+}
+
+/******************************
+*			Vec3
+******************************/
+
+Vec3::Vec3(){
+	x = y = z = 0;
+}
+
+Vec3::Vec3(double _x, double _y, double _z){
+	x = _x;
+	y = _y;
+	z = _z;
+}
+Vec3::Vec3(Vec2 &v){
+	x = v.x;
+	y = v.y;
+	z = 0;
+}
+Vec3::~Vec3(){
+}
+void Vec3::Print(){
+	printf("(%lf, %lf, %lf)\n", x, y, z);
+}
+Vec3 Vec3::Zero(){
+	return Vec3(0, 0, 0);
+}
+Vec3 Vec3::One(){
+	return Vec3(1, 1, 1);
+}
+Vec3 Vec3::XUnit(){
+	return Vec3(1, 0, 0);
+}
+Vec3 Vec3::YUnit(){
+	return Vec3(0, 1, 0);
+}
+Vec3 Vec3::ZUnit(){
+	return Vec3(0, 0, 1);
+}
+
+/******************************
+*			Vec4
+******************************/
+
+Vec4::Vec4(){
+	x = y = z = w = 0;
+}
+
+Vec4::Vec4(double _x, double _y, double _z, double _w){
+	x = _x;
+	y = _y;
+	z = _z;
+	w = _w;
+}
+Vec4::Vec4(Vec2 &v){
+	x = v.x;
+	y = v.y;
+	z = w = 0;
+}
+Vec4::Vec4(Vec3 &v){
+	x = v.x;
+	y = v.y;
+	z = v.z;
+	w = 0;
+}
+Vec4::~Vec4(){
+}
+void Vec4::Print(){
+	printf("(%lf, %lf, %lf, %lf)\n", x, y, z, w);
+}
+Vec4 Vec4::Zero(){
+	return Vec4(0, 0, 0, 0);
+}
+Vec4 Vec4::One(){
+	return Vec4(1, 1, 1, 1);
+}
+Vec4 Vec4::XUnit(){
+	return Vec4(1, 0, 0, 0);
+}
+Vec4 Vec4::YUnit(){
+	return Vec4(0, 1, 0, 0);
+}
+Vec4 Vec4::ZUnit(){
+	return Vec4(0, 0, 1, 0);
+}
+Vec4 Vec4::WUnit(){
+	return Vec4(0, 0, 0, 1);
+}
+
+/******************************
+*			Mat4 
+******************************/
 
 Mat4::Mat4(){
+	mat.resize(16);
 }
 
 
@@ -20,7 +146,7 @@ Mat4::Mat4(double m11, double m12, double m13, double m14,
 Mat4::~Mat4(){
 }
 
-Mat4 Mat4::Rotate(double angle, Vec3 &axis){
+Mat4 Mat4::Rotate(double angle, Vec4 &axis){
     double zAngle = angle *axis.z;
     Mat4 zRot(
         cos(zAngle), -sin(zAngle), 0, 0,
@@ -54,7 +180,7 @@ Mat4 Mat4::Rotate(double angle, Vec3 &axis){
     return ret;
 }
 
-Mat4 Mat4::Translate(Vec3 &t){
+Mat4 Mat4::Translate(Vec4 &t){
     Mat4 m(
         1, 0, 0, t.x,
         0, 1, 0, t.y,
@@ -70,7 +196,15 @@ Mat4 Mat4::Translate(Vec3 &t){
 //    return CMat4_MatMult(tmp,mat);
 }
 
-Mat4 Mat4::Scale(Vec3 &){
+Mat4 Mat4::Scale(Vec4 &v){
+	Mat4 lh = *this;
+	Mat4 m(
+		v.x, 0, 0, 0,
+		0, v.y, 0, 0,
+		0, 0, v.z, 0,
+		0, 0, 0, v.w
+	);
+	return m*lh;
 }
 
 Mat4 Mat4::operator*(Mat4 &rh){
@@ -90,8 +224,8 @@ Mat4 Mat4::operator*(Mat4 &rh){
     return m;
 }
 
-Mat4 Mat4::operator*(Vec4 &rh){
-
+Vec4 Mat4::operator*(Vec4 &rh){
+	return rh;
 }
 
 Mat4 Mat4::operator*(double &rh){
@@ -113,6 +247,20 @@ Mat4 Mat4::operator+(Mat4 &rh){
     return m;
 }
 
+Mat4 Mat4::operator+(double &rh){
+	Mat4 lh = *this;
+
+	Mat4 m;
+	m.mat = {
+		lh.mat[0] + rh, lh.mat[1] + rh, lh.mat[2] + rh, lh.mat[3] + rh,
+		lh.mat[4] + rh, lh.mat[5] + rh, lh.mat[6] + rh, lh.mat[7] + rh,
+		lh.mat[8] + rh, lh.mat[9] + rh, lh.mat[10] + rh, lh.mat[11] + rh,
+		lh.mat[12] + rh, lh.mat[13] + rh, lh.mat[14] + rh, lh.mat[15] + rh
+	};
+
+	return m;
+}
+
 Mat4 Mat4::operator-(Mat4 &rh){
     Mat4 lh = *this;
 
@@ -124,19 +272,19 @@ Mat4 Mat4::operator-(Mat4 &rh){
     return m;
 }
 
-//Mat4& Mat4::operator-(double &rh){
-//    Mat4 lh = *this;
-//
-//    Mat4 m;
-//    m.mat = {
-//         lh.mat[0]-rh,  lh.mat[1]-lh,  lh.mat[2]-rh,  lh.mat[3]-lh,
-//         lh.mat[4]-rh,  lh.mat[5]-lh,  lh.mat[6]-rh,  lh.mat[7]-lh,
-//         lh.mat[8]-rh,  lh.mat[9]-lh, lh.mat[10]-rh, lh.mat[11]-lh,
-//        lh.mat[12]-rh, lh.mat[13]-lh, lh.mat[14]-rh, lh.mat[15]-lh
-//    };
-//
-//    return m;
-//}
+Mat4 Mat4::operator-(double &rh){
+    Mat4 lh = *this;
+
+    Mat4 m;
+    m.mat = {
+         lh.mat[0]-rh,  lh.mat[1]-rh,  lh.mat[2]-rh,  lh.mat[3]-rh,
+         lh.mat[4]-rh,  lh.mat[5]-rh,  lh.mat[6]-rh,  lh.mat[7]-rh,
+         lh.mat[8]-rh,  lh.mat[9]-rh, lh.mat[10]-rh, lh.mat[11]-rh,
+        lh.mat[12]-rh, lh.mat[13]-rh, lh.mat[14]-rh, lh.mat[15]-rh
+    };
+
+    return m;
+}
 
 Mat4 Mat4::Identity(){
     Mat4 m(
@@ -186,4 +334,101 @@ Mat4 Mat4::Perspective(double fov, double aspect, double near, double far){
           0,   0,  -1,   0
     );
     return m;
+}
+
+void Mat4::Print(){
+	std::string str;
+	str = std::to_string(mat[0]);
+	for (unsigned i = 1; i < 16; i++){
+		if ((i % 4) == 0){
+			str += "\n";
+		}
+		else
+		{
+			str += "   ";
+		}
+		str += std::to_string(mat[i]);
+	}
+
+	printf("%s\n", str.c_str());
+}
+
+Mat4& Mat4::operator *= (Mat4 &rh){
+	*this = *this * rh;
+	return *this;
+}
+Mat4& Mat4::operator*=(double &rh){
+	*this = *this * rh;
+	return *this;
+}
+Mat4& Mat4::operator+=(Mat4 &rh){
+	*this = *this + rh;
+	return *this;
+}
+Mat4& Mat4::operator+=(double &rh){
+	*this = *this + rh;
+	return *this;
+}
+Mat4& Mat4::operator-=(Mat4 &rh){
+	*this = *this - rh;
+	return *this;
+}
+Mat4& Mat4::operator-=(double &rh){
+	*this = *this - rh;
+	return *this;
+}
+
+Vec4 operator*(Vec4 &lh, Mat4&rh){
+	Vec4 v;
+	v.x = lh.x*rh.data()[0] + lh.y*rh.data()[4] + lh.z*rh.data()[8] + lh.w*rh.data()[12];
+	v.y = lh.x*rh.data()[1] + lh.y*rh.data()[5] + lh.z*rh.data()[9] + lh.w*rh.data()[13];
+	v.z = lh.x*rh.data()[2] + lh.y*rh.data()[6] + lh.z*rh.data()[10] + lh.w*rh.data()[14];
+	v.w = lh.x*rh.data()[3] + lh.y*rh.data()[7] + lh.z*rh.data()[11] + lh.w*rh.data()[15];
+	return v;
+}
+
+Mat4 operator*(double &lh, Mat4&rh){
+	return rh*lh;
+}
+
+Mat4 operator+(double &lh, Mat4&rh){
+	return rh + lh;
+}
+
+Mat4 operator-(double &lh, Mat4&rh){
+	double m = -1.0;
+	return rh*m + lh;
+}
+
+Matrix::Matrix(unsigned w, unsigned h, std::vector<double> v){
+	if (v.size() > 0){
+		if (v.size() == w*h){
+			printf("%lf", v[0]);
+			for (unsigned i = 1; i < v.size(); i++){
+				if ((i%w) == 0){
+					printf("\n");
+				}
+				else{
+					printf(", ");
+				}
+				printf("%lf", v[i]);
+			}
+			printf("\n");
+		}
+	}
+	else
+	{
+		v.resize(w*h);
+		printf("%lf", v[0]);
+		for (unsigned i = 1; i < v.size(); i++){
+			if ((i%w) == 0){
+				printf("\n");
+			}
+			else{
+				printf(", ");
+			}
+			printf("%lf", v[i]);
+		}
+		printf("\n");
+	}
 }
--- matrix.h
+++ matrix.h
@@ -4,12 +4,13 @@
 #include <vector>
 
 class Vec2{
-protected:
-    double x;
-    double y;
 public:
-    Vec2();
-    ~Vec2();
+	double x;
+	double y;
+	Vec2();
+	Vec2(double _x, double _y);
+	~Vec2();
+	void Print();
     static Vec2 Zero();
     static Vec2 One();
     static Vec2 XUnit();
@@ -22,32 +23,41 @@ public:
     double y;
     double z;
 
-    Vec3();
-    ~Vec3();
-    static Vec2 Zero();
-    static Vec2 One();
-    static Vec2 XUnit();
-    static Vec2 YUnit();
-    static Vec2 ZUnit();
+	Vec3();
+	Vec3(double _x, double _y, double _z);
+	Vec3(Vec2 &);
+	~Vec3();
+	void Print();
+    static Vec3 Zero();
+    static Vec3 One();
+    static Vec3 XUnit();
+    static Vec3 YUnit();
+    static Vec3 ZUnit();
 };
 
 class Vec4{
-protected:
-    double x;
-    double y;
-    double z;
-    double w;
 public:
+	double x;
+	double y;
+	double z;
+	double w;
     Vec4();
+	Vec4(double _x, double _y, double _z, double _w);
     Vec4(Vec2 &);
     Vec4(Vec3 &);
     ~Vec4();
-    static Vec2 Zero();
-    static Vec2 One();
-    static Vec2 XUnit();
-    static Vec2 YUnit();
-    static Vec2 ZUnit();
-    static Vec2 WUnit();
+	void Print();
+    static Vec4 Zero();
+    static Vec4 One();
+    static Vec4 XUnit();
+    static Vec4 YUnit();
+    static Vec4 ZUnit();
+    static Vec4 WUnit();
+};
+
+class Matrix{
+public:
+	Matrix(unsigned w, unsigned h, std::vector<double> v = std::vector<double>());
 };
 
 class Mat4{
@@ -60,21 +70,34 @@ public:
          double, double, double, double,
          double, double,  double, double);
     ~Mat4();
-    Mat4 Rotate(double angle, Vec3 &);
-    Mat4 Translate(Vec3 &);
-    Mat4 Scale(Vec3 &);
+    Mat4 Rotate(double angle, Vec4 &);
+    Mat4 Translate(Vec4 &);
+    Mat4 Scale(Vec4 &);
     Mat4 operator*(Mat4 &rh);
-    Mat4 operator*(Vec4 &rh);
+    Vec4 operator*(Vec4 &rh);
     Mat4 operator*(double &rh);
     Mat4 operator+(Mat4 &rh);
-//    Mat4& operator+(double &rh);
+    Mat4 operator+(double &rh);
     Mat4 operator-(Mat4 &rh);
-//    Mat4& operator-(double &rh);
+	Mat4 operator-(double &rh);
+	Mat4& operator*=(Mat4 &rh);
+	Mat4& operator*=(double &rh);
+	Mat4& operator+=(Mat4 &rh);
+	Mat4& operator+=(double &rh);
+	Mat4& operator-=(Mat4 &rh);
+	Mat4& operator-=(double &rh);
+	const double* data() { return mat.data(); }
+	void Print();
 
     static Mat4 Identity();
     static Mat4 Zero();
     static Mat4 Ortho(double width, double height, double near, double far);
     static Mat4 Perspective(double fov, double aspect, double near, double far);
 };
+
+Vec4 operator*(Vec4 &lh, Mat4&rh);
+Mat4 operator*(double &lh, Mat4&rh);
+Mat4 operator+(double &lh, Mat4&rh);
+Mat4 operator-(double &lh, Mat4&rh);
 
 #endif // MATRIX_H_INCLUDED
